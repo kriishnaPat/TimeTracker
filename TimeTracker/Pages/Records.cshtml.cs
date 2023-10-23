d8da79901ac0ae50781772dc79ef37b21f1ba6e9
Krishna <kriishnapat@gmail.com>
2023-10-22 18:11:56 -0600

    Adding Search Feature to Records Page

--- TimeTracker/Pages/Records.cshtml.cs
+++ TimeTracker/Pages/Records.cshtml.cs
@@ -5,11 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging.Signing;
 using TimeTracker.Models;
 
 namespace TimeTracker.Pages.Students
 {
-	public class RecordsModel : PageModel
+    public class RecordsModel : PageModel
     {
         private readonly TimeIn.Data.TimeInContext _context;
 
@@ -18,14 +19,33 @@ namespace TimeTracker.Pages.Students
             _context = context;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
         public IList<Student> Student { get; set; } = default!;
         public IList<History> StudentIn { get; set; } = default!;
-        public async void OnGet()
+
+        public async Task OnPostAsync()
         {
             var students = from m in _context.History select m;
 
             StudentIn = await students.ToListAsync();
+        }
+
+        public async Task OnGetAsync()
+        {
+            var students = from m in _context.History select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                var student = students.FirstOrDefault(s => s.Date.Contains(SearchString));
+
+            }
+            StudentIn = await students.ToListAsync();
 
         }
+
     }
-}
+
+
+    }
+
